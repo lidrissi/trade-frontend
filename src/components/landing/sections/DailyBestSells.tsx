1e2be46c7d4262247b06199a3d66ca1814493578
LIDRISSI Fatima ezzahra <lidrissi.fatima1ezzahra@gmail.com>
2025-10-28 11:07:36 +0100

    stats section

--- src/components/landing/sections/DailyBestSells.tsx
+++ src/components/landing/sections/DailyBestSells.tsx
@@ -2,7 +2,6 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import clsx from 'clsx';
 import { ArrowUpRight, Bookmark, MoveRight, Star } from 'lucide-react';
-import Image from 'next/image';
 
 export default function DailyBestSells() {
   const navItems = [
@@ -13,164 +12,160 @@ export default function DailyBestSells() {
   ];
 
   return (
-    <div className="container px-6 mx-auto bg-white font-outfit my-6">
-      <nav>
-        <div className="mx-auto flex items-center justify-between py-4">
-          <div className="flex items-center">
-            <h1 className="text-2xl font-bold text-black">Daily Best Sells</h1>
-          </div>
-          <div className="hidden md:flex items-center gap-4">
-            {navItems.map((item, index) => (
-              <a
-                key={index}
-                href="#"
-                className={clsx('text-base font-semibold text-[#253D4E] font-quicksand', item.id === 1 && 'text-cyan')}
-              >
-                {item?.name}
-              </a>
-            ))}
-          </div>
-        </div>
-      </nav>
-      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        <aside className="flex flex-row sm:flex-col justify-between text-white bg-[#3B54DA] rounded-2xl">
-          <div className="py-8 sm:px-10">
-            <h1 className="text-xl sm:text-3xl font-bold">
-              Browse a diverse catalog, designed for international businesses
-            </h1>
-          </div>
-          <div className="mt-2 flex-shrink-1 bg-brand-light1 text-[#000000] p-8 rounded-2xl">
-            <h2 className="text-lg font-bold mb-6 text-nowrap">Explore the catalog</h2>
-            <nav className="space-y-2">
-              <a
-                href="#"
-                className="flex items-center gap-3 text-base font-medium underline hover:no-underline text-nowrap"
-              >
-                <span> All Categories</span>
-                <MoveRight size={16} />
-              </a>
-              <a href="#" className="flex items-center gap-3 text-base font-medium underline hover:no-underline">
-                <span> All Products</span>
-                <MoveRight size={16} />
-              </a>
-              <a href="#" className="flex items-center gap-3 text-base font-medium underline hover:no-underline">
-                <span> All Suppliers</span>
-                <MoveRight size={16} />
-              </a>
-            </nav>
-          </div>
-        </aside>
-        <div className="bg-brand-light1 rounded-3xl overflow-hidden flex flex-col">
-          <div
-            className="rounded-3xl h-65 bg-brand-light1 flex items-center justify-center relative bg-cover bg-center"
-            style={{
-              backgroundImage: `url('/images/home/best-sells-1.png')`,
-            }}
-          >
-            <div className="flex flex-col absolute top-4 right-4 gap-2">
-              <Badge variant={'default'} className="rounded-full font-outfit text-xs">
-                Verified
-              </Badge>
-              <Badge variant={'secondary'} className="rounded-full font-outfit">
-                In stock
-              </Badge>
+    <>
+      <div className="container px-6 mx-auto bg-white font-outfit my-6 rounded-b-3xl">
+        <nav>
+          <div className="mx-auto flex items-center justify-between py-4">
+            <div className="flex items-center">
+              <h1 className="text-2xl font-bold text-black">Daily Best Sells</h1>
+            </div>
+            <div className="hidden md:flex items-center gap-4">
+              {navItems.map((item, index) => (
+                <a
+                  key={index}
+                  href="#"
+                  className={clsx(
+                    'text-base font-semibold text-[#253D4E] font-quicksand',
+                    item.id === 1 && 'text-cyan'
+                  )}
+                >
+                  {item?.name}
+                </a>
+              ))}
             </div>
           </div>
-          <div className="p-8 pt-10 flex-1 flex flex-col">
-            <h3 className="text-lg font-semibold mb-3 text-[#1B2254]">Lorem ipsum dolor sit amet</h3>
-            <p className="text-[#1B2254] text-xs mb-4 leading-relaxed font-inter font-light">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. 
-            </p>
-            <div className="text-base font-semibold mb-2 text-[#314068]">
-              455 DH- 699 DH <span className="text-sm font-normal text-[#6d6a6a]">/ unit</span>
+        </nav>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          <aside className="flex flex-row md:flex-col justify-between text-white bg-[#3B54DA] rounded-2xl">
+            <div className="m-auto px-5 sm:px-10">
+              <h1 className="text-xl sm:text-3xl font-bold">Explore a Diverse Industrial Catalogue</h1>
             </div>
-            <div className="flex items-center gap-1 mb-4 text-xs font-normal">
-              <Star className="w-3 h-3 fill-[#FFB938] text-[#FFB938]" />
-              <span>4,7</span>
-              <span className=" text-[#6d6a6a] font-normal">Lorem ipsum</span>
+            <div className="flex-shrink-1 bg-brand-light1 text-[#000000] p-8 rounded-2xl">
+              <h2 className="text-lg font-bold mb-6 text-nowrap">Explore the catalog</h2>
+              <nav className="space-y-2">
+                <a
+                  href="#"
+                  className="flex items-center gap-3 text-base font-medium underline hover:no-underline text-nowrap"
+                >
+                  <span> All Categories</span>
+                  <MoveRight size={16} />
+                </a>
+                <a href="#" className="flex items-center gap-3 text-base font-medium underline hover:no-underline">
+                  <span> All Products</span>
+                  <MoveRight size={16} />
+                </a>
+                <a href="#" className="flex items-center gap-3 text-base font-medium underline hover:no-underline">
+                  <span> All Suppliers</span>
+                  <MoveRight size={16} />
+                </a>
+              </nav>
             </div>
-            <div className="mt-auto">
-              <div className="flex gap-1">
-                <Button className="bg-cyan hover:bg-cyan/90 text-white text-xs rounded-full font-medium">
-                  Request Quote
-                  <ArrowUpRight className="w-4 h-4 ml-2" />
-                </Button>
-                <Button variant="link" className="text-xs font-underline">
-                  Details
-                </Button>
+          </aside>
+          <div className="bg-brand-light1 rounded-3xl overflow-hidden flex flex-col">
+            <div
+              className="rounded-3xl h-65 bg-brand-light1 flex items-center justify-center relative bg-cover bg-center"
+              style={{
+                backgroundImage: `url('/images/home/best-sells-1.png')`,
+              }}
+            >
+              <div className="flex flex-col absolute top-4 right-4 gap-2">
+                <Badge variant={'default'} className="rounded-full font-outfit text-xs">
+                  Verified
+                </Badge>
+                <Badge variant={'secondary'} className="rounded-full font-outfit">
+                  In stock
+                </Badge>
+              </div>
+            </div>
+            <div className="p-8 pt-10 flex-1 flex flex-col">
+              <h3 className="text-lg font-semibold mb-3 text-[#1B2254]">Lorem ipsum dolor sit amet</h3>
+              <p className="text-[#1B2254] text-xs mb-4 leading-relaxed font-inter font-light">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. 
+              </p>
+              <div className="text-base font-semibold mb-2 text-[#314068]">
+                455 DH- 699 DH <span className="text-sm font-normal text-[#6d6a6a]">/ unit</span>
+              </div>
+              <div className="flex items-center gap-1 mb-4 text-xs font-normal">
+                <Star className="w-3 h-3 fill-[#FFB938] text-[#FFB938]" />
+                <span>4,7</span>
+                <span className=" text-[#6d6a6a] font-normal">Lorem ipsum</span>
+              </div>
+              <div className="mt-auto">
+                <div className="flex gap-1">
+                  <Button className="bg-cyan hover:bg-cyan/90 text-white text-xs rounded-full font-medium">
+                    Request Quote
+                    <ArrowUpRight className="w-4 h-4 ml-2" />
+                  </Button>
+                  <Button variant="link" className="text-xs font-underline">
+                    Details
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="grid gap-6">
-          <div
-            className=" bg-brand-light1 min-h-65 rounded-3xl overflow-hidden flex flex-col justify-end relative bg-no-repeat bg-cover bg-center"
-            style={{
-              backgroundImage: `url('/images/home/best-sells-3.png')`,
-            }}
-          >
-            <div className="absolute top-6 right-6 z-10">
-              <Bookmark className="w-6 h-6 fill-[#FFB938] text-[#FFB938]" />
+          <div className="grid grid-cols-2 md:grid-cols-1 gap-6">
+            <div className=" bg-brand-light1 min-h-35 md:min-h-65 rounded-3xl overflow-hidden flex flex-col justify-end relative bg-no-repeat bg-cover bg-center bg-[linear-gradient(to_top,rgba(0,0,0,0.7)_0%,transparent_50%),url('/images/home/best-sells-3.png')]">
+              <div className="absolute top-6 right-6 z-10">
+                <Bookmark className="w-6 h-6 fill-[#FFB938] text-[#FFB938]" />
+              </div>
+              <h3 className="text-lg font-semibold text-white p-6"> Lorem ipsum dolor sit amet consectetur </h3>
             </div>
-            <h3 className="text-lg font-semibold text-white p-6">ffff Lorem ipsum dolor sit amet consectetur </h3>
-          </div>
-          <div
-            className="min-h-65 bg-brand-light1 rounded-3xl overflow-hidden flex flex-col justify-end relative bg-no-repeat bg-cover bg-center"
-            style={{
-              backgroundImage: `url('/images/home/best-sells-4.png')`,
-            }}
-          >
-            <div className="absolute top-6 right-6 z-10">
-              <Bookmark className="w-6 h-6 fill-[#FFB938] text-[#FFB938]" />
-            </div>
-            <h3 className="text-lg font-semibold text-white p-6">Lorem ipsum dolor sit amet consectetur </h3>
-          </div>
-        </div>
-        <div className="bg-brand-light1 rounded-3xl overflow-hidden flex flex-col">
-          <div
-            className="rounded-3xl h-65 bg-brand-light1 flex items-center justify-center relative bg-cover bg-center"
-            style={{
-              backgroundImage: `url('/images/home/best-sells-2.png')`,
-            }}
-          >
-            <div className="flex flex-col absolute top-4 right-4 gap-2">
-              <Badge variant={'default'} className="rounded-full font-outfit text-xs">
-                Verified
-              </Badge>
-              <Badge variant={'secondary'} className="rounded-full font-outfit">
-                In stock
-              </Badge>
+            <div className="min-h-35 md:min-h-65 bg-brand-light1 rounded-3xl overflow-hidden flex flex-col justify-end relative bg-no-repeat bg-cover bg-cente bg-[linear-gradient(to_top,rgba(0,0,0,0.7)_0%,transparent_50%),url('/images/home/best-sells-4.png')]">
+              <div className="absolute top-6 right-6 z-10">
+                <Bookmark className="w-6 h-6 fill-[#FFB938] text-[#FFB938]" />
+              </div>
+              <h3 className="text-lg font-semibold text-white p-6">Lorem ipsum dolor sit amet consectetur </h3>
             </div>
           </div>
-          <div className="p-8 pt-10 flex-1 flex flex-col">
-            <h3 className="text-lg font-semibold mb-3 text-[#1B2254]">Lorem ipsum dolor sit amet</h3>
-            <p className="text-[#1B2254] text-xs mb-4 leading-relaxed font-inter font-light">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-              dolore magna aliqua. 
-            </p>
-            <div className="text-base font-semibold mb-2 text-[#314068]">
-              455 DH- 699 DH <span className="text-sm font-normal text-[#6d6a6a]">/ unit</span>
+          <div className="bg-brand-light1 rounded-3xl overflow-hidden flex flex-col">
+            <div
+              className="rounded-3xl h-65 bg-brand-light1 flex items-center justify-center relative bg-cover bg-center"
+              style={{
+                backgroundImage: `url('/images/home/best-sells-2.png')`,
+              }}
+            >
+              <div className="flex flex-col absolute top-4 right-4 gap-2">
+                <Badge variant={'default'} className="rounded-full font-outfit text-xs">
+                  Verified
+                </Badge>
+                <Badge variant={'secondary'} className="rounded-full font-outfit">
+                  In stock
+                </Badge>
+              </div>
             </div>
-            <div className="flex items-center gap-1 mb-4 text-xs font-normal">
-              <Star className="w-3 h-3 fill-[#FFB938] text-[#FFB938]" />
-              <span>4,7</span>
-              <span className=" text-[#6d6a6a] font-normal">Lorem ipsum</span>
-            </div>
-            <div className="mt-auto">
-              <div className="flex gap-1">
-                <Button className="bg-cyan hover:bg-cyan/90 text-white text-xs rounded-full font-medium">
-                  Request Quote
-                  <ArrowUpRight className="w-4 h-4 ml-2" />
-                </Button>
-                <Button variant="link" className="text-xs font-underline">
-                  Details
-                </Button>
+            <div className="p-8 pt-10 flex-1 flex flex-col">
+              <h3 className="text-lg font-semibold mb-3 text-[#1B2254]">Lorem ipsum dolor sit amet</h3>
+              <p className="text-[#1B2254] text-xs mb-4 leading-relaxed font-inter font-light">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. 
+              </p>
+              <div className="text-base font-semibold mb-2 text-[#314068]">
+                455 DH- 699 DH <span className="text-sm font-normal text-[#6d6a6a]">/ unit</span>
+              </div>
+              <div className="flex items-center gap-1 mb-4 text-xs font-normal">
+                <Star className="w-3 h-3 fill-[#FFB938] text-[#FFB938]" />
+                <span>4,7</span>
+                <span className=" text-[#6d6a6a] font-normal">Lorem ipsum</span>
+              </div>
+              <div className="mt-auto">
+                <div className="flex gap-1">
+                  <Button className="bg-cyan hover:bg-cyan/90 text-white text-xs rounded-full font-medium">
+                    Request Quote
+                    <ArrowUpRight className="w-4 h-4 ml-2" />
+                  </Button>
+                  <Button variant="link" className="text-xs font-underline">
+                    Details
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className="w-full h-16 bg-[#1E1B68]">
+        <div className="bg-white rounded-b-3xl h-14" />
+      </div>
+    </>
   );
 }
--- src/components/landing/sections/Stats.tsx
+++ src/components/landing/sections/Stats.tsx
@@ -1,4 +1,4 @@
-import { ImageIcon } from 'lucide-react';
+import Image from 'next/image';
 
 export default function Stats() {
   const stats = [
@@ -21,18 +21,15 @@ export default function Stats() {
   ];
 
   return (
-    <div className="container px-6 mx-auto  font-quicksand my-15">
-      <div className="grid grid-cols-2 lg:grid-cols-4 justify-between  bg-[#292929] rounded-xl sm:rounded-3xl p-6 sm:px-12 sm:py-18 gap-4">
+    <div className="font-quicksand bg-[#1E1B68]">
+      <div className="container mx-auto grid grid-cols-2 lg:grid-cols-4 justify-between p-6 sm:px-6 sm:py-20">
         {stats.map((stat, index) => (
-          <div key={index} className="flex items-start gap-6">
-            <div className="flex-shrink-0 w-8 h-8 sm:w-16 sm:h-16 bg-[#d9d9d9] rounded-lg sm:rounded-2xl flex items-center justify-center">
-              <ImageIcon className="w-4 h-4 sm:w-8 sm:h-8 text-[#292929]" strokeWidth={1.5} />
-            </div>
+          <div key={index} className="flex items-center">
+            <Image alt="" width={110} height={110} src={`/images/home/stats${index + 1}.png`} />
             <div className="flex flex-col">
               <h3 className="text-[#ffffff] text-xl  sm:text-5xl font-bold leading-none">{stat.value}</h3>
               <p className="text-[#ffffff] text-xs font-bold leading-relaxed">{stat.label}</p>
             </div>
-            {index !== stats.length - 1 && <div className="hidden lg:blockh-full w-0.25 bg-white" />}
           </div>
         ))}
       </div>
